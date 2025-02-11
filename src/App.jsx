446006230b5f7752984c961ded5cfaa27bbb12ff
ljcl79 <ljcl79@gmail.com>
2025-02-11 20:15:52 -0300

    Deploy automático

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React / Actualizado y se va a hacer el deploy automático</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
